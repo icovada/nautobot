0244d57c923f62459a7c5c9eb8f66d3ffa05049e
Federico Tabbò <federico.tabbo@networktocode.com>
2025-08-20 16:39:38 +0000

    Upgrade all changelogs, including those with no target object

--- nautobot/extras/migrations/helpers/new_objectchange.py
+++ nautobot/extras/migrations/helpers/new_objectchange.py
@@ -4,14 +4,19 @@ from django.db import connection
 from nautobot.extras.models.change_logging import ChangeLoggedModel, ObjectChange
 
 
-
 def make_funcs_for_model(app_name, model_name):
     def forward(apps, schema_editor):
         Model: ChangeLoggedModel = apps.get_model(app_name, model_name)
         ModelObjectChange = apps.get_model(app_name, f"{model_name}ObjectChange")
 
         for obj in ObjectChange.objects.filter(changed_object_type=ContentType.objects.get_for_model(Model)):
-            if obj.changed_object_id is not None:
+            if obj.changed_object is None:
+                with connection.cursor() as cursor:
+                    cursor.execute(
+                        f"INSERT INTO {ModelObjectChange._meta.db_table} (changelog_ptr_id, target_uuid) VALUES (%s, %s)",
+                        [obj.pk, obj.changed_object_id],
+                    )
+            else:
                 with connection.cursor() as cursor:
                     cursor.execute(
                         f"INSERT INTO {ModelObjectChange._meta.db_table} (changelog_ptr_id, target_uuid, fktarget_id) VALUES (%s, %s, %s)",
