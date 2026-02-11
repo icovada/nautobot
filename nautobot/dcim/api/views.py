149235c68f3af40f31a1edd7a0044f5f3e3a5616
Federico Tabbò <federico.tabbo@networktocode.com>
2026-02-11 20:31:35 +0000

    cable_termination, not cable

--- nautobot/dcim/api/views.py
+++ nautobot/dcim/api/views.py
@@ -603,31 +603,31 @@ class DeviceViewSet(ConfigContextQuerySetMixin, NautobotModelViewSet):
 
 
 class ConsolePortViewSet(PathEndpointMixin, NautobotModelViewSet):
-    queryset = ConsolePort.objects.prefetch_related("cable_ends__cable__cable_ends__termination")
+    queryset = ConsolePort.objects.prefetch_related("cable_ends__cable__cable_ends__cable_termination")
     serializer_class = serializers.ConsolePortSerializer
     filterset_class = filters.ConsolePortFilterSet
 
 
 class ConsoleServerPortViewSet(PathEndpointMixin, NautobotModelViewSet):
-    queryset = ConsoleServerPort.objects.prefetch_related("cable_ends__cable__cable_ends__termination")
+    queryset = ConsoleServerPort.objects.prefetch_related("cable_ends__cable__cable_ends__cable_termination")
     serializer_class = serializers.ConsoleServerPortSerializer
     filterset_class = filters.ConsoleServerPortFilterSet
 
 
 class PowerPortViewSet(PathEndpointMixin, NautobotModelViewSet):
-    queryset = PowerPort.objects.prefetch_related("cable_ends__cable__cable_ends__termination")
+    queryset = PowerPort.objects.prefetch_related("cable_ends__cable__cable_ends__cable_termination")
     serializer_class = serializers.PowerPortSerializer
     filterset_class = filters.PowerPortFilterSet
 
 
 class PowerOutletViewSet(PathEndpointMixin, NautobotModelViewSet):
-    queryset = PowerOutlet.objects.prefetch_related("cable_ends__cable__cable_ends__termination")
+    queryset = PowerOutlet.objects.prefetch_related("cable_ends__cable__cable_ends__cable_termination")
     serializer_class = serializers.PowerOutletSerializer
     filterset_class = filters.PowerOutletFilterSet
 
 
 class InterfaceViewSet(PathEndpointMixin, NautobotModelViewSet):
-    queryset = Interface.objects.prefetch_related("cable_ends__cable__cable_ends__termination").annotate(
+    queryset = Interface.objects.prefetch_related("cable_ends__cable__cable_ends__cable_termination").annotate(
         _ip_address_count=count_related(IPAddress, "interfaces")  # avoid conflict with Interface.ip_address_count()
     )
     serializer_class = serializers.InterfaceSerializer
@@ -737,7 +737,7 @@ class PowerPanelViewSet(NautobotModelViewSet):
 
 
 class PowerFeedViewSet(PathEndpointMixin, NautobotModelViewSet):
-    queryset = PowerFeed.objects.prefetch_related("cable_ends__cable__cable_ends__termination")
+    queryset = PowerFeed.objects.prefetch_related("cable_ends__cable__cable_ends__cable_termination")
     serializer_class = serializers.PowerFeedSerializer
     filterset_class = filters.PowerFeedFilterSet
 
